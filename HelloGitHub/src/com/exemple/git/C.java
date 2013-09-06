14d046cf198a00784b4cc483429eb889922c60d7
JIHANE <JIHANE@JIHANE-PC>
2013-09-06 21:06:34 +0100

    ajout d'une nouvelle classe

---
+++ HelloGitHub/src/com/exemple/git/C.java
@@ -0,0 +1,14 @@
+package com.exemple.git;
+
+public class C {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
