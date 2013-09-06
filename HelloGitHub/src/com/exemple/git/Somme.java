1ee4b79786997f326f5ae6389c08c6565b9b2361
JIHANE <JIHANE@JIHANE-PC>
2013-09-06 20:58:46 +0100

    projet de test

---
+++ HelloGitHub/src/com/exemple/git/Somme.java
@@ -0,0 +1,17 @@
+package com.exemple.git;
+
+public class Somme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		int r=a+b;
+		System.out.println("somme ="+r);
+
+	}
+
+}
